43a4ebb8ea562e047758edddc2a6c303d4a7a51b
Myllena Navarro <128384896+Myllena-navarro@users.noreply.github.com>
2025-05-07 01:35:06 -0300

    Update teste_conexao.py

--- teste_conexao.py
+++ teste_conexao.py
@@ -2,16 +2,11 @@ from db_conexao import conectar
 import pyodbc
 
 def test_conectar():
-    """
-    Testa a função de conexão ao banco de dados SQL Server.
-    """
     conn = conectar()
     assert conn is not None, "A conexão não deve ser None."
 
-    # Verifica se o tipo de conexão é pyodbc.Connection
     assert isinstance(conn, pyodbc.Connection), "O tipo de conexão deve ser pyodbc.Connection."
 
-    # Fecha a conexão após o teste
     conn.close()
 
 if __name__ == "__main__":
