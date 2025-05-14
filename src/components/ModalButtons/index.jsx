4b44a840807e04833bd7a4d4752750eb2a5b7943
TavaressDev <tutstavares@outlook.com>
2025-05-14 00:28:48 -0300

    commit modal funcionario

--- src/components/ModalButtons/index.jsx
+++ src/components/ModalButtons/index.jsx
@@ -2,6 +2,6 @@
 
 export const ModalButtons = ({onClick, text}) => {
     return(
-        <button className={`${text == "Cancelar" ? "bg-white border text-[#414651] border-[#D5D7DA]" : "bg-[#C0090E] text-white"} font-bold text-[16px] rounded-lg w-full py-2.5 flex justify-center items-center`} onClick={onClick}>{text}</button>
+        <button className={`${text == "Cancelar" ? "bg-white border text-[#414651] border-[#D5D7DA] cursor-pointer" : "bg-[#C0090E] text-white"} font-bold text-[16px] rounded-lg w-full py-2.5 flex justify-center items-center cursor-pointer`} onClick={onClick}>{text}</button>
     )
 }
